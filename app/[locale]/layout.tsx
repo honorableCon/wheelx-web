5a876575e53be6903dfbd95010cadab133a30cef
Honorable Con <34490054+honorableCon@users.noreply.github.com>
2026-03-05 00:49:30 +0000

    feat: add vercel analytics

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -6,6 +6,7 @@ import { NextIntlClientProvider } from 'next-intl';
 import { getMessages } from 'next-intl/server';
 import { notFound } from 'next/navigation';
 import { locales } from '@/navigation';
+import { Analytics } from '@vercel/analytics/next'
 
 const outfit = Outfit({
   variable: "--font-outfit",
@@ -103,6 +104,7 @@ export default async function RootLayout({
           <JsonLd />
           {children}
         </NextIntlClientProvider>
+        <Analytics/>
       </body>
     </html>
   );
